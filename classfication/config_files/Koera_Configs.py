127cd9f8d89338b645153399555f8de7c57f3046
Deyu Li <162010106+stevenstage@users.noreply.github.com>
2025-10-08 21:06:42 +0800

    Create Koera_Configs.py

---
+++ classfication/config_files/Koera_Configs.py
@@ -0,0 +1,67 @@
+class Config(object):
+    def __init__(self):
+        # model configs
+        self.input_channels = 1
+        self.increased_dim = 1
+        self.onelayer_out_channels = 64
+        self.twolayer_out_channels = 128
+        self.final_out_channels = 256
+        self.conv2_kernel_size = 8
+        self.conv3_kernel_size = 8
+        self.d_ff = 256
+        self.num_classes = 15
+        self.num_classes_target = 15
+        self.dropout = 0.2
+        self.masking_ratio = 0.5
+        self.lm = 3
+        self.kernel_size = 25
+        self.stride = 3
+        self.features_len = 172
+        self.features_len_f = self.features_len
+        self.TSlength_aligned = 512
+        self.ft_dropout = 0.2
+        self.CNNoutput_channel = 10
+        # training configs
+        self.num_epoch = 40
+
+        # optimizer parameters
+        self.optimizer = 'adam'
+        self.beta1 = 0.9
+        self.beta2 = 0.99
+        self.lr = 3e-8
+        self.lr_f = self.lr
+
+        # data parameters
+        self.drop_last = True
+        self.batch_size = 64
+
+        self.target_batch_size = 32
+        self.Context_Cont = Context_Cont_configs()
+        self.TC = TC()
+        self.augmentation = augmentations()
+
+        self.negative_nums = 1
+        self.positive_nums = 3
+        self.n_knlg = 8
+        self.head_dropout = 0.2
+        self.hidden_dimension = 1280
+
+
+
+class augmentations(object):
+    def __init__(self):
+        self.jitter_scale_ratio = 1.5
+        self.jitter_ratio = 2
+        self.max_seg = 12
+
+
+class Context_Cont_configs(object):
+    def __init__(self):
+        self.temperature = 0.2
+        self.use_cosine_similarity = True
+
+
+class TC(object):
+    def __init__(self):
+        self.hidden_dim = 64
+        self.timesteps = 50
